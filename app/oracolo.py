b1a3e1eb495fd3a51fb2fad4df8bb240a9a23f38
Matteo <118370682+MatteoAdamo82@users.noreply.github.com>
2022-12-01 16:12:00 +0100

    wip

--- app/oracolo.py
+++ app/oracolo.py
@@ -5,10 +5,11 @@ from datetime import datetime
 import sys
 
 csvFile = 'data/estrazioni-lotto.csv'
-v1 = sys.argv[1]
-v2 = sys.argv[2]
-
 ruota = {'BA': 1, 'CA': 2, 'FI': 3, 'GE': 4, 'MI': 5, 'NA': 6, 'PA': 7, 'RO': 8, 'TO': 9, 'VE': 10, 'NZ': 11}
+
+v1 = sys.argv[1]
+v2 = ruota[sys.argv[2].upper()]
+
 custom_date_parser = lambda x: datetime.strptime(x, "%d/%m/%Y")
 estrazioni = read_csv(csvFile, delimiter=';', keep_default_na=False, parse_dates=['data'],date_parser=custom_date_parser)
 
@@ -23,4 +24,4 @@ model.fit(X.values, y.values)
 
 predictNumbers = model.predict([[v1,v2]])
 
-print(predictNumbers)
+print(predictNumbers[0])
